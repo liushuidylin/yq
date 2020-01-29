bb754f05a282c9a94d2d5fc1c059a0a582abc245
duyilin <duyilin@golaxy.cn>
2020-01-29 14:30:13 +0800

    增加每日新增数据功能

--- db.py
+++ db.py
@@ -12,15 +12,33 @@ class DB:
     def find_all(self):
         print(self.db.list_collection_names())
 
+    def statistics_data(self, date):
+        q = {"insertTime": date}
+        return self.db['yq'].find(q)
+
+    def find_one(self, q):
+        return self.db['yq'].find_one(q)
+
 
 if __name__ == '__main__':
     db = DB()
-    test_data = {
-        'province': 'shanxi',
-        'city': 'yuncheng',
-        'confirmed': 2,
-        'death': 0,
-        'cured': 0,
-        'suspect': 1
-    }
-    db.insert('yq', test_data)
+    new_date = '2020-01-29 00:00:00'
+    old_date = '2020-01-28 00:00:00'
+    data = db.statistics_data(new_date)
+    new_data = ''
+    for d in data:
+        if 'cityName' in d:
+            q = {"provinceName": d['provinceName'], "cityName": d['cityName'], "insertTime": old_date}
+            new_data = db.find_one(q)
+        else:
+            q = {"provinceName": d['provinceName'], "insertTime": old_date}
+            new_data = db.find_one(q)
+
+        if new_data is None:
+            pass
+        else:
+            d['confirmedCount'] = d['confirmedCount'] - new_data['confirmedCount']
+            d['suspectedCount'] = d['suspectedCount'] - new_data['suspectedCount']
+            d['curedCount'] = d['curedCount'] - new_data['curedCount']
+            d['deadCount'] = d['deadCount'] - new_data['deadCount']
+            db.insert('data', d)
